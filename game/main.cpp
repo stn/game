18beb252c2ad7050050a3adfc0334e4ce76db658
Akira Ishino <akira.ishino@gmail.com>
2012-06-28 22:52:25 +0900

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by Akira Ishino on 12/06/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
